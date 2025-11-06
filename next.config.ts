65778292d56e30b8d86578558222c385d1165bf1
ghost2023 <nathnaelwonisha@gmail.com>
2025-11-06 17:29:20 +0300

    fix: config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 import moduleAnalzer from "@next/bundle-analyzer";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+};
 
 const withBundleAnalyzer = moduleAnalzer({
   enabled: process.env.ANALYZE === "true",
